5768fa1bf719edf96e736fffa3d1a0c7dfccad76
Daniel Schlachter <daniel.schlachter@sap.com>
2021-08-13 14:39:16 +0000

    updated dependencies

--- srv/server.js
+++ srv/server.js
@@ -1,9 +1,9 @@
 const cds = require("@sap/cds");
 try {
     console.log("Hello Agent");
-    require('@dynatrace/oneagent')();
-    require('@dynatrace/oneagent-sdk')();
+    //require('@dynatrace/oneagent')();
+    //require('@dynatrace/oneagent-sdk')();
 } catch (err) {
     console.log('Failed to load OneAgent: ', err);
-}
+} 
 module.exports = cds.server;
